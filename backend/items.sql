f038e5b76252180d99686d9bea25d0a1bbe3f7dc
fed-blip <dimakyobas@gmail.com>
2025-12-23 14:20:35 +0000

    feat: оновлено items.sql — додано товари аксесуарів

--- backend/items.sql
+++ backend/items.sql
@@ -1,1 +1,10 @@
+CREATE TABLE IF NOT EXISTS items(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
 
+INSERT INTO items(name) VALUES
+ ('Навушникии'),
+ ('Чохол для телефона'),
+ ('Зарядний кабель'),
+ ('Повербанк');
